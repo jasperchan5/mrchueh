2f2327c6e163a9f90e73153867da27ddd2c6d213
jasperchan5 <jscnn51011@gmail.com>
2022-04-04 20:45:28 +0800

    Bug fixed

--- frontend/src/Containers/Funcs/FuncsIndex.js
+++ frontend/src/Containers/Funcs/FuncsIndex.js
@@ -1,8 +1,5 @@
 import CovidInfo from "./CovidFuncs/CovidInfo";
-import GodLanguage from "./HentaiFuncs/GodLanguage";
-import Tags from "./HentaiFuncs/Tags";
 import Board from "./TicTacToeFuncs.js/Board";
 import ChatBox from "./SolitaireFuncs/ChatBox";
-import Normal from "./ImageFuncs/Normal";
 
-export { CovidInfo, GodLanguage, Tags, Board, ChatBox, Normal };
+export { CovidInfo, Board, ChatBox };
